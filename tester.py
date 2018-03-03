eea660212edd3afbcd373023319b572024712846
Ted Michael Celis <krazete@gmail.com>
2018-03-02 20:54:50 -0800

    removed the training step from the tester module

--- tester.py
+++ tester.py
@@ -6,15 +6,15 @@ def get_majority(prediction_list, index):
     majority = Counter(prediction).most_common()
     return majority[0][0]
 
-def predict_ensemble(train_X, train_Y, test_X, classifiers):
+def predict_ensemble(test_X, classifiers):
     predictions = []
     ensemble_predictions = []
-    for clf in classifiers:
-        classifier = clf(train_X, train_Y)
-        predictions.append(classifier.predict(test_X))
+    for classifier in classifiers:
+        prediction = classifier.predict(test_X)
+        predictions.append(prediction)
 
     for i in range(test_X.shape[0]):
-        ensemble_predictions.append(get_majority(predictions,i))
+        ensemble_predictions.append(get_majority(predictions, i))
     return ensemble_predictions
 
 def error_ratio(Y_hat, Y):
@@ -24,7 +24,7 @@ def error_ratio(Y_hat, Y):
             error += 1
     return error / Y.shape[0]
 
-def train_predict_error(train_X, train_Y, test_X, test_Y, classifer):
-    prediction = classifer(train_X, train_Y).predict(test_X)
+def predict_error(test_X, test_Y, classifer):
+    prediction = classifer.predict(test_X)
     error = error_ratio(prediction, test_Y)
     print(error)
