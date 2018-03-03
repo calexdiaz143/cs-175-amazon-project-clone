952b604dabaeef16bd185178ffa6a09b7a1b26df
Ted Michael Celis <krazete@gmail.com>
2018-03-02 20:52:34 -0800

    changed .read() to .readlines() (for Books), added parameters to load()

--- loader.py
+++ loader.py
@@ -4,13 +4,18 @@ import numpy as np
 import json
 import parser
 
-def get_category(path, percent):
-    json_reviews = open(path).read().split('\n')[:500]#[:-1]
+def get_category(path, percent, cutoff=-1):
+    content = open(path)
     reviews = []
-    for json_review in json_reviews:
+    for json_review in content.readlines():
         raw_review = json.loads(json_review)
         review = parser.parse_review(raw_review)
-        reviews.append(review)
+        if review[2] >= 0.5:
+            reviews.append(review)
+        
+        if cutoff == 0:
+            break
+        cutoff -= 1
 
     shuffle(reviews)
 
@@ -19,13 +24,13 @@ def get_category(path, percent):
     test = np.array(reviews[split_index:])
     return train, test
 
-def get_categories(categories):
+def get_categories(categories, percent, cutoff=-1):
     train_reviews = []
     train_categories = []
     test_reviews = []
     test_categories = []
     for category in categories:
-        train, test = get_category('db/reviews_' + category + '_5.json', 0.75)
+        train, test = get_category('db/reviews_' + category + '_5.json', percent, cutoff)
         train_reviews.append(train)
         train_categories.append([category] * train.shape[0])
         test_reviews.append(test)
@@ -50,14 +55,15 @@ def save(train_X, train_Y, test_X, test_Y):
     save_npz('saved/test_X', test_X)
     np.save('saved/test_Y', train_Y)
 
-def load(categories, use_saved = False):
+def load(categories, percent=0.75, cutoff=-1, use_saved=False, overwrite_saved=True):
     if use_saved:
         train_X = load_npz('saved/train_X.npz')
         train_Y = np.load('saved/train_Y.npy')
         test_X = load_npz('saved/test_X.npz')
         test_Y = np.load('saved/test_Y.npy')
     else:
-        train_X, train_Y, test_X, test_Y = get_categories(categories)
+        train_X, train_Y, test_X, test_Y = get_categories(categories, percent, cutoff)
         train_X, test_X = parser.parse_BOW(train_X, test_X)
-        save(train_X, train_Y, test_X, test_Y)
+        if overwrite_saved:
+            save(train_X, train_Y, test_X, test_Y)
     return train_X, train_Y, test_X, test_Y
