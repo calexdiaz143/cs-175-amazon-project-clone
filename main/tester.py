51cfccedd702df93ce4175bb15aa0398722ec57d
Cesar Alex Diaz <calexdiaz143@gmail.com>
2018-03-16 23:05:43 -0700

    Finished Normalized confusion matrix function
    -Not sure where to put this data
    -Thinking of removing the data for correct guesses from the matrix then
    instead normalizing only on the incorrect guesses because right now
    they don’t visually give too much information

--- main/tester.py
+++ main/tester.py
@@ -1,5 +1,8 @@
 from __future__ import division
 from collections import Counter
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_majority(prediction_list, index):
     prediction = [pred[index] for pred in prediction_list]
@@ -18,3 +21,18 @@ def error_ratio(Y, predictions):
         if y != prediction:
             error += 1
     return error / Y.shape[0]
+
+# Normalized Confusion Matrix: Heavily based on the scikit-learn example here:
+# http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def disp_conf_matrix(Y, Y_hat, classes):
+    cm = confusion_matrix(Y, Y_hat,labels=classes)
+    cm = cm.astype('float')/cm.sum(axis=1)[:, np.newaxis]
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    plt.show()
