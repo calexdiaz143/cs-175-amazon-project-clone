8e42cfd64fb8021cbfb9f5057794871fdaf0c1bf
Ted Michael Celis <krazete@gmail.com>
2018-03-18 11:34:59 -0700

    added docstrings

--- main/tester.py
+++ main/tester.py
@@ -5,17 +5,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def get_majority(prediction_list, index):
+    '''Returns the majority vote of a single datapoint using a list of predictions made by an ensemble of classifiers.'''
     prediction = [pred[index] for pred in prediction_list]
     majority = Counter(prediction).most_common()
     return majority[0][0]
 
 def predict_ensemble(X, prediction_list):
+    '''Returns the majority vote of all data in X using a list of predictions made by an ensemble of classifiers.'''
     ensemble_predictions = []
     for i in range(X.shape[0]):
         ensemble_predictions.append(get_majority(prediction_list, i))
     return ensemble_predictions
 
 def error_ratio(Y, predictions):
+    '''Returns the error rate of a list of predictions given a list of the true labels.'''
     error = 0
     for y, prediction in zip(Y, predictions):
         if y != prediction:
@@ -25,6 +28,7 @@ def error_ratio(Y, predictions):
 # Normalized Confusion Matrix with zero'd out diagonal: Heavily based on the scikit-learn example here:
 # http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
 def disp_conf_matrix(Y, Y_hat, classes):
+    '''Displays a confusion matrix given a list of predictions, a list of the true labels, and a list of the corresponding classifiers.'''
     cm = confusion_matrix(Y, Y_hat,labels=classes)
     diagonal = np.diag_indices_from(cm)
     cm[diagonal] = 0
