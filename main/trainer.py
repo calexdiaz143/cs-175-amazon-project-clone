9c40daad48794d283f958864b8dba09f1d59b763
Ted Michael Celis <krazete@gmail.com>
2018-03-18 11:30:48 -0700

    added docstrings

--- main/trainer.py
+++ main/trainer.py
@@ -6,18 +6,21 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 
 def naive_bayes(X, Y):
+    '''Trains and returns a Multinomial Naive Bayes classifier with the given data and labels.'''
     # Default Parameters
     #   alpha = 1
     classifier = MultinomialNB(alpha=0.0001).fit(X, Y)
     return classifier
 
 def bernoulli_naive_bayes(X, Y):
+    '''Trains and returns a Bernoulli Naive Bayes classifier with the given data and labels.'''
     # Default Parameters
     #   alpha = 1
     classifier = BernoulliNB(alpha=0.0001).fit(X, Y)
     return classifier
 
 def logistic_regression(X, Y):
+    '''Trains and returns a Logistic Regression classifier with the given data and labels.'''
     # Default Parameters
     #   penalty = 'l2'
     #   C = 1
@@ -25,18 +28,21 @@ def logistic_regression(X, Y):
     return classifier
 
 def svm(X, Y):
+    '''Trains and returns a Support Vector Machine classifier with the given data and labels.'''
     # Default Parameters
     #   x
     classifier = SVC().fit(X, Y)
     return classifier
 
 def knn(X, Y):
+    '''Trains and returns a K-Nearest Neighbors classifier with the given data and labels.'''
     # Default Parameters
     #   x
     classifier = KNeighborsClassifier(n_neighbors=1000, weights='distance').fit(X, Y)
     return classifier
 
 def mlp(X, Y):
+    '''Trains and returns a Multilayer Perceptron classifier with the given data and labels.'''
     # Default Parameters
     #   hidden_layer_sizes = (100,)
     #   alpha = 0.0001
@@ -45,6 +51,7 @@ def mlp(X, Y):
     return classifier
 
 def random_forest(X, Y):
+    '''Trains and returns a Random Forest classifier with the given data and labels.'''
     # Default Parameters
     #   n_estimators = 10
     #   max_features = 'auto'
@@ -55,6 +62,7 @@ def random_forest(X, Y):
     return classifier
 
 def adaboost(X, Y):
+    '''Trains and returns an AdaBoost classifier with the given data and labels.'''
     # Default Parameters
     #   n_estimators = 50
     #   learning_rate = 1
@@ -62,6 +70,7 @@ def adaboost(X, Y):
     return classifier
 
 def gradient_boost(X, Y):
+    '''Trains and returns a Gradient Boosting classifier with the given data and labels.'''
     # Default Parameters
     #   n_estimators = 100
     #   max_depth = 3
